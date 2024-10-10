eda63b84b483d453e8ffcfdbfcca66157666dc59
Ambogo2 <ruthambogo.ra@gmail.com>
2024-10-10 18:15:11 +0300

    initial commit

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+function getListStudents() {
+  // Define an array of objects with students' information
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return students;
+}
