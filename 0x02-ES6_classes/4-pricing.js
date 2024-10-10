41c4bfadadfaf9bfe87e3c6e00c1d21de489b2ae
Ambogo2 <ruthambogo.ra@gmail.com>
2024-10-11 01:04:29 +0300

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+import Currency from './3-currency';
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount; // Store amount
+    this._currency = currency; // Store currency (Currency instance)
+  }
+
+  // Getter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  // Setter for amount
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // Getter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter for currency
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  // Method to display the full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
